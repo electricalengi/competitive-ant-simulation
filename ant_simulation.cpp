27604448d5f9c73dd06226162debe535bb8aa8c7
electricalengi <62599748+electricalengi@users.noreply.github.com>
2023-08-29 21:48:05 +1000

    created simulation tick functionality

--- ant_simulation.cpp
+++ ant_simulation.cpp
@@ -16,7 +16,7 @@ public:
 	float nestScent;
 	int foodSourceNumber;
 
-	Patch(int c = 0; int f = 0, bool n = false, int s = 0, int fsn = 0) : chemical(c), food(f), nest(n), nestScent(s), foodSourceNumber(fsn) {}
+	Patch(int c = 0, int f = 0, bool n = false, int s = 0, int fsn = 0) : chemical(c), food(f), nest(n), nestScent(s), foodSourceNumber(fsn) {}
 };
 
 class Soldier {
@@ -42,7 +42,7 @@ public:
 	bool hasFood;
 
 
-	Worker(int startX, int startY, int fac, bool hf, ) : x(startX), y(startY), colony(fac), hasFood(hf) {}
+	Worker(int startX, int startY, int fac, bool hf) : x(startX), y(startY), colony(fac), hasFood(hf) {}
 
 	void lookForFood(const std::vector<Patch>& patches) {
 		// todo
@@ -60,35 +60,39 @@ public:
 
 
 class Simulation {
-public
-	Worker workers[populationSize];
-	Patch patches[display_size, display_size];
+public:
+		Worker workers[populationSize];
+	Patch patches[display_size][display_size];
+	int tick = 0;
+
+
+	int nestx = display_size / 2;
+	int nesty = display_size / 2;
+	int food_1_x = 100;
+	int food_1_y = 100;
 	
-	
-	int nestx, nesty = display_size/2, display_size/2;
-	int food_1_x, food_1_y = 100, 100;
-	
+
 	Simulation() {
 		initialiseWorkers();
 		initialisePatches();
 	}
-	
+
 	void initialisePatches() {
-		for (int x = 0; x < display_size, x++) {
-			for (int y = 0; y < display_size, y++) {
+		for (int x = 0; x < display_size; x++) {
+			for (int y = 0; y < display_size; y++) {
 				Patch& patch = patches[x][y];
-				distance_nest = pow(nestx - x, 2) + pow(nesty - y, 2);
-				distance_food = pow(food_1_x - x, 2) + pow(food_1_y - y, 2);
-				
+				float distance_nest = pow(nestx - x, 2) + pow(nesty - y, 2);
+				float distance_food = pow(food_1_x - x, 2) + pow(food_1_y - y, 2);
+
 				if (distance_food < 30) {
 					patch.foodSourceNumber = 1;
 					patch.food = 10;
 				}
-				
+
 				if (distance_nest < 50) {
 					patch.nest = true;
 				}
-				
+
 				patch.nestScent = 1 / distance_nest;
 			}
 		}
@@ -96,12 +100,22 @@ public
 
 	void initialiseWorkers() {
 		for (int index = 0; index < populationSize; index++) {
-			Worker worker = workers[index];
+			Worker& worker = workers[index];
 			worker.x = 0;
 			worker.y = 0;
 		}
 	}
 
 	void go() {
-		//todo
+		if (tick < maxTicks) {
+			for (int index = 0; index < populationSize; index++) {
+				Worker& worker = workers[index];
+				if (worker.hasFood) {
+					worker.returnToNest(patches);
+				}
+				else { worker.lookForFood(patches); }
+			}
+			tick = tick + 1;
+		}
 	}
+}
